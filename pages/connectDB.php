b8a7fcd3bc3c66570d4146ff701402289710d614
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-14 16:10:11 +0700

    it
    more function in connectDB

--- pages/connectDB.php
+++ pages/connectDB.php
@@ -18,12 +18,45 @@ class DB_conn
         }
     }
 
-    function insert_user(/*$fname, $lname, $mname, $email, $username, $password, $type*/ $id, $user, $pass, $first, $last, $tele)
+    function insert_user(/*$fname, $lname, $mname, $telephone, $username, $password, $type*/ $id, $user, $pass, $first, $last, $tele)
     {
         $sql = "insert into user(	id	,username	,password	,first_name	,last_name	,telephone)
             values('$id', '$user', '$pass', '$first', '$last', '$tele')";
         return ($sql);
     }
+    public function display_user()
+    {
+        $str = mysqli_query($this->conn, "SELECT * from user");
+        return $str;
+    }
+    public function display_user_edit($id)
+    {
+        $str = mysqli_query($this->conn, "SELECT * from user where id = $id");
+        return $str;
+    }
+    public function edit_user($fname, $lastname, $telephone, $id)
+    {
+        $str = mysqli_query($this->conn, "UPDATE user SET first_name = '$fname', last_name ='$lastname',telephone = '$telephone' WHERE id = $id ");
+        return $str;
+    }
+    public function del_user($id)
+    {
+        $str = mysqli_query($this->conn, "DELETE FROM user WHERE user_ID = $id ");
+        return $str;
+    }
+    // ---------------------products --------------------
+    public function select_category(){
+        $strSQL = "SELECT * FROM category ORDER BY c_name ASC";
+        $str = mysqli_query($this->conn, $strSQL);
+        return $str;
+    }
+    public function insert_product($p_name,$p_detail,$p_price,$p_category,$path_img){
+        $strSQL = "INSERT INTO products (pName, pDetail, pPrice,c_id, pImage)
+        values ('$p_name', '$p_detail',$p_price,$p_category,'$path_img')";
+        $str = mysqli_query($this->conn,$strSQL);
+        return $str;
+    }
+
 }
 
 ?>
