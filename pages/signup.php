a1782d3130a6413865331c0d544ce32fb5a8c2bb
DeliciousBoy <outdata.gide@gmail.com>
2023-03-14 17:30:13 +0700

    Update signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -10,6 +10,9 @@ session_start();
 		//something was posted
 		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$telephone = $_POST['telephone'];
 		
 		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
 		{
