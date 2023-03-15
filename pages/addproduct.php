6d24cf3a64d0570179f02458fbd542c5dd54965b
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2023-03-15 19:08:45 +0700

    bug fix

--- pages/addproduct.php
+++ pages/addproduct.php
@@ -34,41 +34,42 @@ $conn = new DB_conn;
 <body>
     <div class="container-fluid pt-5">
         <h2 class="text-center mb-4"><span class="px-2">Add products</span></h2>
-        <form method="POST" action="insert_product.php" enctype="multipart/form-data" >
-        <div class="row px-xl-5">
-            <div class="col-lg-7 mb-5">
-            <div class="mb-3">
-                <label class="help-block text-danger">Name of product:</label>
-                <input type="text" class="form-control" id="id" name="id">
-            </div>
-            <div class="mb-3">
-                <label class="help-block text-danger">Product details:</label>
-                <input type="text" class="form-control" id="name" name="name">
-            </div>
-            <div class="mb-3">
-                <label class="help-block text-danger">Prices: </label>
-                <input type="text" class="form-control" id="price" name="price">
-            </div>
-            <div class="mb-3">
-                <label class="help-block text-danger">Product image:</label>
-                    <input type="file" class="form-control" id="picture" name="picture">
-            </div>
-            <div class="mb-3">
-                <select class="form-control" name="category" id="category ">
-                    <option value=""><-- Choose product types --></option>
-            </div>
-            </div>
-            <?php
-                    $sql = $conn->select_category();
-                    while ($data = mysqli_fetch_array($sql)) { ?>
-                        <option value="<?php echo $data['c_id'];?>"> <?php echo $data['c_name']; ?> </option> 
-                        <?php 
-                        } 
-                        ?>
-                </select>
+        <form method="POST" action="insert_product.php" enctype="multipart/form-data">
+            <div class="row px-xl-5">
+                <div class="col-lg-7 mb-5">
+                    <div class="mb-3">
+                        <label class="help-block text-danger">Name of product:</label>
+                        <input type="text" class="form-control" id="id" name="id">
+                    </div>
+                    <div class="mb-3">
+                        <label class="help-block text-danger">Product details:</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label class="help-block text-danger">Prices: </label>
+                        <input type="text" class="form-control" id="price" name="price">
+                    </div>
+                    <div class="mb-3">
+                        <label class="help-block text-danger">Product image:</label>
+                        <input type="file" class="form-control" id="picture" name="picture">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-control" name="category" id="category">
+                            <option value=""><-- Choose product types --></option>
+                            <?php
+                            $sql = $conn->select_category();
+                            while ($data = mysqli_fetch_array($sql)) { ?>
+                                <option value="<?php echo $data['c_id']; ?>"> <?php echo $data['c_name']; ?> </option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
             </div>
             <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">SAVE</button>
         </form>
     </div>
 </body>
+
 </html>
