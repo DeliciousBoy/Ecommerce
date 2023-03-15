b58d6f378357936532eae4d38b7b490554bbbd60
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-15 17:50:00 +0700

    Update deluser.php

--- pages/deluser.php
+++ pages/deluser.php
@@ -4,4 +4,10 @@ $conn = new DB_conn; //สร้าง object ชื่อ $condb
 ?>
 <?php
 $sql = $conn->del_user($_GET['id']);
+if ($sql) {
+    echo "<script>alert('ลบข้อมูลสําเร็จ')</script>";
+    echo "<script>window.location.href='display.php' </script>";
+} else {
+    echo "<script>alert('เกิดข้อผิดพลาด')</script>";
+}
 ?>
