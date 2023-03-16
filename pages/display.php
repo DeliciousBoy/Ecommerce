adbe069480f03a437a387556c5ff7460bb5dca98
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-16 15:17:51 +0700

    Update display.php

--- pages/display.php
+++ pages/display.php
@@ -5,29 +5,33 @@ $conn = new DB_conn;
 <!DOCTYPE html>
 <html lang="en">
 <style>
-table {
-  border-collapse: collapse;
-  width: 100%;
-  
-}
-body{
-    min-heheight: 100vh;
-}
+    table {
+        border: top -100%; ;
+        border-collapse: collapse;
+        width: 90%;
+        margin-left: 5%;
 
-th, td{
-  text-align: center;
-  padding: 8px;
-}
-th {
-  background-color: #00CB3D;
-  color: white;
-}
+    }
 
-tr:nth-child(odd) {
-  background-color: #DAFFE9;
+    body {
+        min-height: 100vh;
+    }
 
-}
+    th,
+    td {
+        text-align: center;
+        padding: 8px;
+    }
 
+    th {
+        background-color: #212F3D;
+        color: white;
+    }
+
+    tr:nth-child(odd) {
+        background-color: #FEF9E7;
+
+    }
 </style>
 
 <body>
@@ -37,45 +41,45 @@ tr:nth-child(odd) {
     <div class="table-header">
         <div class="table">
             <!-- <div class="col-lg-8 table-responsive mb-5"> -->
-                <!-- <table class="table table-bordered text-center mb-0" style="width:100%"> -->
-                <table cellpadding = "100" cellspacing = "100" border="10" > 
-                    <thead class="bg-secondary text-dark">
+            <!-- <table class="table table-bordered text-center mb-0" style="width:100%"> -->
+            <table cellpadding="100" cellspacing="100" border="10">
+                <thead class="bg-secondary text-dark">
+                    <tr>
+                        <th colspan="7">
+                            <h3>ข้อมูล สมาชิก</h3>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>ลําดับ</th>
+                        <th>ชื่อ</th>
+                        <th>นามสกุล</th>
+                        <th>username</th>
+                        <th>telephone_number</th>
+                        <th>แก้ไข</th>
+                        <th>ลบ</th>
+                    </tr>
+                </thead>
+                <tbody class="align-middle">
+                    <?php
+                    $sql = $conn->display_user();
+                    $i = 1;
+                    while ($data = mysqli_fetch_array($sql)) {
+                    ?>
                         <tr>
-                            <th colspan="7">
-                                <h3>ข้อมูล สมาชิก</h3>
-                            </th>
+                            <td class="align-middle"><?php echo $i ?> </td>
+                            <td class="align-middle"><?php echo $data['first_name'] ?></td>
+                            <td class="align-middle"><?php echo $data['last_name'] ?></td>
+                            <td class="align-middle"><?php echo $data['username'] ?></td>
+                            <td class="align-middle"><?php echo $data['telephone'] ?></td>
+                            <td class="align-middle"><a href="userEdit.php?id=<?php echo $data['id'] ?>" class="btn btn-sm btn-primary">Edit</a></td>
+                            <td class="align-middle"><a href="deluser.php?id=<?php echo $data['id'] ?>" class="btn btn-sm btn-primary" onclick="return confirm ('คุณต้องการลบข้อมูลใช่หรือไม่')"><i class="fa fa-times"></i></a></td>
                         </tr>
-                        <tr>
-                            <th>ลําดับ</th>
-                            <th>ชื่อ</th>
-                            <th>นามสกุล</th>
-                            <th>username</th>
-                            <th>telephone_number</th>
-                            <th>แก้ไข</th>
-                            <th>ลบ</th>
-                        </tr>
-                    </thead>
-                    <tbody class="align-middle">
-                        <?php
-                        $sql = $conn->display_user();
-                        $i = 1;
-                        while ($data = mysqli_fetch_array($sql)) {
-                        ?>
-                            <tr>
-                                <td class="align-middle"><?php echo $i ?> </td>
-                                <td class="align-middle"><?php echo $data['first_name'] ?></td>
-                                <td class="align-middle"><?php echo $data['last_name'] ?></td>
-                                <td class="align-middle"><?php echo $data['username'] ?></td>
-                                <td class="align-middle"><?php echo $data['telephone'] ?></td>
-                                <td class="align-middle"><a href="userEdit.php?id=<?php echo $data['id'] ?>" class="btn btn-sm btn-primary">Edit</a></td>
-                                <td class="align-middle"><a href="deluser.php?id=<?php echo $data['id'] ?>" class="btn btn-sm btn-primary" onclick="return confirm ('คุณต้องการลบข้อมูลใช่หรือไม่')"><i class="fa fa-times"></i></a></td>
-                            </tr>
-                        <?php $i = $i + 1;
-                        } ?>
-                    </tbody>
-                </table>
-            </div>
+                    <?php $i = $i + 1;
+                    } ?>
+                </tbody>
+            </table>
         </div>
+    </div>
     </div>
 </body>
 
