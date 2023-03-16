900219cd0284ad0bea59f277c581b42295f68ffa
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-16 20:53:39 +0700

    Update addproduct.php

--- pages/addproduct.php
+++ pages/addproduct.php
@@ -12,133 +12,53 @@ $conn = new DB_conn;
         form {
             text-align: left;
             margin: auto;
-            margin-left: 10%;
-            margin-right: -45%;
+            margin-left: 5%;
+            margin-right: 5%;
         }
     </style>
 </head>
 
 <body>
-    <?php 
+    <?php
     include_once('topbar.php');
     ?>
     <div class="container-fluid pt-5">
-        <div class="text-center mb-4">
-            <h2 class="section-title px-5"><span class="px-2">Add Products</span></h2>
-        </div>
-        <div class="row px-xl-5">
-            <div class="col-lg-7 mb-5">
-                <div class="contact-form">
-                    <div id="success"></div>
-                    <form name="product" id="product" novalidate="novalidate" action="insertPro.php" enctype="multipart/form-data" method="post" text-align: center;>
-                        <div class="control-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Product Name" required oninvalid="setCustomValidity('Please enter your first name')" oninput="setCustomValidity('')" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <input type="text" class="form-control" id="details" name="pDetails" placeholder="Product details" required oninvalid="setCustomValidity('Please enter your last name')" oninput="setCustomValidity('')" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <input type="text" class="form-control" id="price" name="price" placeholder="Price of product" required oninvalid="setCustomValidity('Please enter your username name')" oninput="setCustomValidity('')" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <input type="file" class="form-control" id="picture" name="picture" placeholder="picture" required oninvalid="setCustomValidity('Please enter your password')" oninput="setCustomValidity('')" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="mb-3">
-                            <select class="form-control" name="category" id="category ">
-                                <option value=""><-- กรุณาเลือกประเภท --></option>
-                                <?php
-                                $sql = $conn->select_category();
-                                while ($data = mysqli_fetch_array($sql)) { ?>
-                                    <option value="<?php $data['c_id']; ?>"> <?php echo $data['c_name']; ?> </option>
-                                <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">Send Message</button>
-                        </div>
-                    </form>
         <h2 class="text-center mb-4"><span class="px-2">Add products</span></h2>
-        <form method="POST" action="insert_product.php" enctype="multipart/form-data">
-            <div class="row px-xl-5">
-                <div class="col-lg-7 mb-5">
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Name of product:</label>
-                        <input type="text" class="form-control" id="id" name="id" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Product details:</label>
-                        <input type="text" class="form-control" id="name" name="name" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Prices: </label>
-                        <input type="text" class="form-control" id="price" name="price" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Product image:</label>
-                        <input type="file" class="form-control" id="picture" name="picture" require>
-                    </div>
-                    <div class="mb-3">
-                        <select class="form-control" name="category" id="category">
-                            <option value="">-- Choose product types--</option>
-                            <?php
-                            $sql = $conn->select_category();
-                            while ($data = mysqli_fetch_array($sql)) { ?>
-                                <option> <?php echo $data['c_name']; ?> </option>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="mb-3" align = 'center'>
-                    <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">SAVE</button>
-                    </div>
-                </div>
+        <form name="product" id="product" novalidate="novalidate" action="insertPro.php" enctype="multipart/form-data" method="post">
+            <div class="control-group">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Product Name" required oninvalid="setCustomValidity('Please enter your first name')" oninput="setCustomValidity('')" />
+                <p class="help-block text-danger"></p>
             </div>
-           
-        <h2 class="text-center mb-4"><span class="px-2">Add products</span></h2>
-        <form method="POST" action="insert_product.php" enctype="multipart/form-data">
-            <div class="row px-xl-5">
-                <div class="col-lg-7 mb-5">
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Name of product:</label>
-                        <input type="text" class="form-control" id="id" name="id" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Product details:</label>
-                        <input type="text" class="form-control" id="name" name="name" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Prices: </label>
-                        <input type="text" class="form-control" id="price" name="price" require>
-                    </div>
-                    <div class="mb-3">
-                        <label class="help-block text-danger">Product image:</label>
-                        <input type="file" class="form-control" id="picture" name="picture" require>
-                    </div>
-                    <div class="mb-3">
-                        <select class="form-control" name="category" id="category">
-                            <option value="">-- Choose product types--</option>
-                            <?php
-                            $sql = $conn->select_category();
-                            while ($data = mysqli_fetch_array($sql)) { ?>
-                                <option> <?php echo $data['c_name']; ?> </option>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="mb-3" align = 'center'>
-                    <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">SAVE</button>
-                    </div>
-                </div>
+            <div class="control-group">
+                <input type="text" class="form-control" id="details" name="pDetails" placeholder="Product details" required oninvalid="setCustomValidity('Please enter your last name')" oninput="setCustomValidity('')" />
+                <p class="help-block text-danger"></p>
             </div>
-        </form>
+            <div class="control-group">
+                <input type="text" class="form-control" id="price" name="price" placeholder="Price of product" required oninvalid="setCustomValidity('Please enter your username name')" oninput="setCustomValidity('')" />
+                <p class="help-block text-danger"></p>
+            </div>
+            <div class="control-group">
+                <input type="file" class="form-control" id="picture" name="picture" placeholder="picture" required oninvalid="setCustomValidity('Please enter your password')" oninput="setCustomValidity('')" />
+                <p class="help-block text-danger"></p>
+            </div>
+            <div class="mb-3">
+                <select class="form-control" name="category" id="category">
+                    <option value="">-- Choose product types--</option>
+                    <?php
+                    $sql = $conn->select_category();
+                    while ($data = mysqli_fetch_array($sql)) { ?>
+                        <option value="<?php $data['c_id']; ?>"> <?php echo $data['c_name']; ?> </option>
+                    <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="mb-3" align='center'>
+                <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">SAVE</button>
+            </div>
+    </div>
+    </div>
+    </form>
     </div>
 </body>
 
