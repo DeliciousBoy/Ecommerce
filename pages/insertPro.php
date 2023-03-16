052e28be95947a4b164c332023b317e8fb9fb3a6
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-16 22:13:06 +0700

    Update insertPro.php

--- pages/insertPro.php
+++ pages/insertPro.php
@@ -1,35 +1,34 @@
 
 <?php
-ini_set('file_uploads','1');
+ini_set('file_uploads', '1');
 include_once("connectDB.php");
 $conn = new DB_conn;
 // $con = $conndb->conn;
 
-$p_name = $_POST['name']; 
-$p_details = $_POST['details']; 
-$p_price = $_POST['price']; 
+$p_name = $_POST['name'];
+$p_details = $_POST['pDetails'];
+$p_price = $_POST['price'];
 $p_category = $_POST['category']; //------File upload-----
 $name = $_FILES['picture']['name']; #'test.jpg' 
 $tmp_name = $_FILES['picture']['tmp_name'];
 
 #-------------Finding file extension--------
-$position= strpos($name, "."); 
-$fileextension= substr($name, $position + 1); 
-$fileextension= strtolower($fileextension); 
+$position = strpos($name, ".");
+$fileextension = substr($name, $position + 1);
+$fileextension = strtolower($fileextension);
 echo $fileextension;
-if($fileextension == 'jpg' or $fileextension == 'png'){
+if ($fileextension == 'jpg' or $fileextension == 'png') {
     if (isset($name)) {
         $path = '../p_img/';
-         if(!empty($name)){ 
-            if(move_uploaded_file($tmp_name,$path.$name)){
-            $path_img = $path . $name; 
-            $sql = $conn->insert_product($p_name,$p_details,$p_price,$path_img,$p_category);
-            if($sql){
-                echo "<script>alert('Your product has been saved!')</script>"; 
-                echo "<script>window.location='add_product.php'</script>";
-            }
-            else { 
-                echo "<script>alert( Error: " . $sql . ":-" . mysqli_error($conn).")</script>";
+        if (!empty($name)) {
+            if (move_uploaded_file($tmp_name, $path . $name)) {
+                $path_img = $path . $name;
+                $sql = $conn->insert_product($p_name, $p_details, $p_price, $p_category,$path_img);
+                if ($sql) {
+                    echo "<script>alert('Your product has been saved!')</script>";
+                    echo "<script>window.location='addproduct.php'</script>";
+                } else {
+                    echo "<script>alert( Error: " . $sql . ":-" . mysqli_error($conn) . ")</script>";
                 }
             }
         }
