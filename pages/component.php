6b6f98ec3993c631bdd9674f2374ac44d152b173
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2023-03-27 22:40:17 +0700

    เอาชื่อสินค้ากลับมาหน้า shop

--- pages/component.php
+++ pages/component.php
@@ -12,11 +12,11 @@ function component($p_id, $p_name, $p_details, $p_price, $path_img){
                                 <a href="#" class="img-prod"><img class="img-flup_id" src="' .$path_img. '" alt="Product Image"></a>
                                 <div class="text py-3 pb-4 px-3">
                                 <input type="hidden" name="pName" value="' .$p_name. '">
-
-                                    <p class="pDetails">' .$p_details. '</p>
-                                    <p class="pPrice">Price: ' .$p_price. '</p>
-                                    <input type="hidden" name="p_id" value="' .$p_id. '">
-                                    <button class="btn btn-primary py-2 px-4" type="submit" name="p_add" id="p_add">Add to Cart</button>
+                                <h3 class="pName">' .$p_name. '</h3>
+                                <p class="pDetails">' .$p_details. '</p>
+                                <p class="pPrice">Price: ' .$p_price. '</p>
+                                <input type="hidden" name="p_id" value="' .$p_id. '">
+                                <button class="btn btn-primary py-2 px-4" type="submit" name="p_add" id="p_add">Add to Cart</button>
                                 </div>
                             </div>
                         </div>
