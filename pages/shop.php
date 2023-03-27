891318164c8b106fa1b5710826ea0f484ef5d6cf
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-28 00:06:50 +0700

    it

--- pages/shop.php
+++ pages/shop.php
@@ -33,8 +33,6 @@ if (isset($_POST['p_add'])) {
 
 
 
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,42 +51,24 @@ include_once('topbar.php')
 </div>
 
 <!--Products-->
+<section class="ftco-section bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-10 order-md-last">
+                    <div class="row">
 <?php
 while ($data = mysqli_fetch_array($sql)) {
     echo component($data['p_id'], $data['pName'], $data['pDetails'], $data['pPrice'], $data['pImage']);
 }
-
 ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
 
 <?php
 include_once('footer.php');
 ?>
-
-
-
-<!-- loader -->
-<div pName="ftco-loader" class="show fullscreen"><svg class="circular" wpNameth="48px" height="48px">
-        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-wpNameth="4" stroke="#eeeeee" />
-        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-wpNameth="4" stroke-miterlimit="10" stroke="#F96D00" />
-    </svg></div>
-
-
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery-migrate-3.0.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/jquery.waypoints.min.js"></script>
-<script src="js/jquery.stellar.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/aos.js"></script>
-<script src="js/jquery.animateNumber.min.js"></script>
-<script src="js/bootstrap-datepicker.js"></script>
-<script src="js/scrollax.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-<script src="js/google-map.js"></script>
-<script src="js/main.js"></script>
-
 
 </html>
