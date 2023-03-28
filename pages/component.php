3da5d5873117fb6a55939a643e720559ce56118a
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-03-28 22:19:25 +0700

    Update component.php

--- pages/component.php
+++ pages/component.php
@@ -2,8 +2,7 @@
 
 function component($p_id, $p_name, $p_details, $p_price, $path_img)
 {
-    $element = '<form action="#" method="post" enctype="multipart/form-data" >
-    <div class="col-sm-12 col-md-12 col-lg-4 ftco-animate d-flex">
+    $element = '<div class="col-sm-12 col-md-12 col-lg-4 ftco-animate d-flex">
     <div class="product d-flex flex-column">
         <a href="#" class="img-prod"><img class="img-fluid img-thumbnail" src="' . $path_img . '" alt="Colorlib Template">
             <div class="overlay"></div>
@@ -13,10 +12,8 @@ function component($p_id, $p_name, $p_details, $p_price, $path_img)
             <p class="pDetails">' . $p_details . '</p>
             <p class="pPrice">Price: ' . $p_price . '</p>
             <input type="hidden" name="p_id" value="' . $p_id . '">
-            <p class="bottom-area d-flex px-3">
                 <button class="add-to-cart text-center py-2 mr-1" type="submit" name="p_add" id="p_add"><span>Add to cart <i class="ion-ios-add ml-1"></i></span></button>
                 <button class="buy-now text-center py-2" type="submit" name="p_add" id="p_add"><a href="checkout.php">Buy now</a><span><i class="ion-ios-cart ml-1"></i></span></button>
-            </p>
         </div>
     </div>
 </div>';
