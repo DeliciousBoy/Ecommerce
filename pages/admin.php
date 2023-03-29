431f54d3c39e76bbf4a2038c7ad0cb312c9c66fe
DeliciousBoy <outdata.gide@gmail.com>
2023-03-29 13:35:57 +0700

    เพิ่มถ้า logout ไปแล้วจะไม่สามารถไปหน้า admin ได้

--- pages/admin.php
+++ pages/admin.php
@@ -1,8 +1,8 @@
 <?php 
     include("connectDB.php");
-    $conndb = new DB_conn; 
-    $con = $conndb->conn; 
-    
+    // $conndb = new DB_conn; 
+    // $con = $conndb->conn; 
+    // session_start();
     if(!isset($_SESSION['admin_login'])){
         $_SESSION['warning'] = 'กรุณาเข้าสู่ระบบ';
         header('location: login.php');
--- pages/admin_tables.php
+++ pages/admin_tables.php
@@ -1,4 +1,4 @@
-<?php include_once('admin.php'); ?>
+<?php require_once('admin.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
     
@@ -111,8 +111,8 @@
                 </div>
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" action = "logout.php"  type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.php">Logout</a>
+                    <button class="btn btn-secondary"  type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
                 </div>
             </div>
         </div>
