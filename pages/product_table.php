6079c08cd12e437f3c7f34d1901393c130b706d8
DeliciousBoy <outdata.gide@gmail.com>
2023-04-01 21:30:11 +0700

    edit product แก้จาก id เป็น ชื่อแทน

--- pages/product_table.php
+++ pages/product_table.php
@@ -15,7 +15,19 @@
             <td class="align-middle"><?php echo $data['pDetails'] ?></td>
             <td class="align-middle"><?php echo $data['pPrice'] ?></td>
             <td class="align-middle"><?php echo $data['pImage'] ?></td>
-            <td class="align-middle"><?php echo $data['id'] ?></td>
+            <td class="align-middle"><?php
+                if ($data['id'] == 1) {
+                    echo 'Basket';
+                } else if ($data['id'] == 2) {
+                    echo 'Chair';
+                } else if ($data['id'] == 3) {
+                    echo 'Lamp';
+                } else if ($data['id'] == 4) {
+                    echo 'Bowl';
+                } else if ($data['id'] == 5) {
+                    echo 'Hat';
+                }
+            ?></td>
             <td class="align-middle"><a href="admin_product_edit.php?id=<?php echo $data['p_id'] ?>" class="btn btn-sm btn-primary">Edit</a></td>
             <td class="align-middle"><a href="deluser.php?id=<?php echo $data['p_id'] ?>" class="btn btn-sm btn-primary" onclick="return confirm ('คุณต้องการลบข้อมูลใช่หรือไม่')"><i class="fa fa-times"></i></a></td>
         </tr>
@@ -23,8 +35,3 @@
     } ?>
 </tbody>
 
-<script>
-    if (id==1){
-
-    }
-</script>
