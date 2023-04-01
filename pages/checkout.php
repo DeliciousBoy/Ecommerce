0e840a3a380aeab395e85b2bb87fd1f744ae6dbe
DeliciousBoy <outdata.gide@gmail.com>
2023-04-01 15:38:21 +0700

    Update checkout.php

--- pages/checkout.php
+++ pages/checkout.php
@@ -112,7 +112,12 @@ include_once('topbar.php')
                 </div>
 	            </div>
 	        </form>
-
+				<?php
+				$subtotal = $_SESSION['checkout'];
+				$discount = 0;
+				$delivery = 0;
+				$total = $subtotal-$discount;
+				?>
 				<div class="row mt-5 pt-3 d-flex">
 					<div class="col-md-6 d-flex">
 						<div class="cart-detail cart-total bg-light p-3 p-md-4">
@@ -176,73 +181,6 @@ include_once('topbar.php')
 	</div>
 </section> <!-- .section -->
 
-	          <div class="row mt-5 pt-3 d-flex">
-	          	<div class="col-md-6 d-flex">
-	          		<div class="cart-detail cart-total bg-light p-3 p-md-4">
-	          			<h3 class="billing-heading mb-4">Cart Total</h3>
-	          			<p class="d-flex">
-		    						<span>Subtotal</span>
-		    						<span>$20.60</span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Delivery</span>
-		    						<span>$0.00</span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Discount</span>
-		    						<span>$3.00</span>
-		    					</p>
-		    					<hr>
-		    					<p class="d-flex total-price">
-		    						<span>Total</span>
-		    						<span>1</span>
-		    					</p>
-								</div>
-	          	</div>
-	          	<div class="col-md-6">
-	          		<div class="cart-detail bg-light p-3 p-md-4">
-	          			<h3 class="billing-heading mb-4">Payment Method</h3>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="radio">
-											   <label><input type="radio" name="optradio" class="mr-2"> Direct Bank Tranfer</label>
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="radio">
-											   <label><input type="radio" name="optradio" class="mr-2"> Check Payment</label>
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="radio">
-											   <label><input type="radio" name="optradio" class="mr-2"> Paypal</label>
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="checkbox">
-											   <label><input type="checkbox" value="" class="mr-2"> I have read and accept the terms and conditions</label>
-											</div>
-										</div>
-									</div>
-									<button class="btn btn-primary py-2 px-4 " form = "user_address" name="user_address" type="submit" id="user_address">Submit</button>
-									
-									<!-- <p><a href="order_bill.php"class="btn btn-primary py-3 px-4">Place an order</a></p> -->
-								</div>
-							</div>
-						</div>
-						
-					</div> <!-- .col-md-8 -->
-				</div>
-			</div>
-		</section> <!-- .section -->
-		
-		<!-- <button class="btn btn-primary py-2 px-4 " form = "user_address" name="user_address" type="submit" id="user_address">Submit</button> -->
 
 <?php
 include_once('footer.php');
