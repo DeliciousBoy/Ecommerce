ff2f26fd6edafd8347924959ec6730304929602e
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2023-04-01 07:59:26 +0700

    ตระกร้าเพิ่มเลขตามจำนวนด้วย

--- pages/topbar.php
+++ pages/topbar.php
@@ -53,7 +53,7 @@
 						<?php
 						if (isset($_SESSION['cart'])) {
 							$count = count($_SESSION['cart']);
-							echo '<spand id = "cart_count">' . $count . '</spand>';
+							echo '<spand id = "cart_count">' . ($count+$quantity-1) . '</spand>';
 						} else {
 							echo '<spand id="cart_count">0</spand>';
 						}
