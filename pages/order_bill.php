7056a4d90c48bfb31a841049a2ceb5ac05d22fd7
DeliciousBoy <outdata.gide@gmail.com>
2023-04-02 04:59:15 +0700

    Update order_bill.php

--- pages/order_bill.php
+++ pages/order_bill.php
@@ -17,21 +17,21 @@
     // $order = $_SESSION['cart'];
     
     
-    echo 'user_id: ',$user_id ,' ','name: ',$first_name, $last_name, ' address: ' ,$address_line1, $address_line2,'  city: ' ,$city," postal_code: " ,$postal_code
-    ,' country: ', $country, " mobile: ",$mobile;
+    // echo 'user_id: ',$user_id ,' ','name: ',$first_name, $last_name, ' address: ' ,$address_line1, $address_line2,'  city: ' ,$city," postal_code: " ,$postal_code
+    // ,' country: ', $country, " mobile: ",$mobile;
 
     $sql = $conndb->insert_order($user_id,$address_line1,$address_line2,$city,$postal_code,$country,$mobile);
-    if(mysqli_query($con,$sql)){
-        printf("%d Row insert. \n",mysqli_affected_rows($con));
-    }
-    
-    // if($sql){
-    //     echo "<script>alert('add order สำเร็จ')</script>";
-    //     echo "<script>window.location.href='login.php' </script>";
-    // }else{
-    //     echo "<script>alert('เกิดข้อผิดพลาด')</script>";
-    //     echo "<script>window.location.href='signup.php' </script>";
+    // if(mysqli_query($con,$sql)){
+    //     printf("%d Row insert. \n",mysqli_affected_rows($con));
     // }
+    
+    if($sql){
+        echo "<script>alert('add order สำเร็จ')</script>";
+        echo "<script>window.location.href='shop.php' </script>";
+    }else{
+        echo "<script>alert('เกิดข้อผิดพลาด')</script>";
+        echo "<script>window.location.href='shop.php' </script>";
+    }
        
     
     mysqli_close($con);
