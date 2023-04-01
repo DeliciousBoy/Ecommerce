24bb67d1040a554d238110e1d439564ec46185f5
ittidet <86585134+ittidet12@users.noreply.github.com>
2023-04-01 19:22:16 +0700

    it

--- pages/cart.php
+++ pages/cart.php
@@ -86,7 +86,12 @@ include_once('topbar.php')
                             <h3>Cart Totals</h3>
                             <p class="d-flex total-price">
                                 <span>total</span>
-                                <span><?php echo $total ?></span>
+                                <span><?php if(isset($total)){
+                                    echo $total;
+                                }else{
+                                    $total =0;
+                                    echo $total;
+                                }  ?></span>
                             </p>
                         </div>
                         <?php
